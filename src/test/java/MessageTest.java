1e5b1e787ce6c0d245d0bf2adb81d44169e67e4a
bro-scientist <jacxta@gmail.com>
2020-12-19 19:28:01 +0100

    #28 basic tests for example

--- src/test/java/MessageTest.java
+++ src/test/java/MessageTest.java
@@ -1,3 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class MessageTest
 {
+
+    @BeforeEach
+    void prepare()
+    {
+        // keine Ahnung
+    }
+
+    @Test
+    void message_should_contain_given_text()
+    {
+        String testText = "Hi, wie geht's?";
+        Message testMessage = new Message(testText);
+        Assertions.assertEquals(testText, testMessage.getText());
+    }
+
+    /**
+     * example for a failing test.
+     */
+    @Test
+    void message_should_contain_given_text_but_this_one_will_fail()
+    {
+        String testText = "Hi, wie geht's?";
+        Message testMessage = new Message(testText + " nö");
+        Assertions.assertEquals(testText, testMessage.getText());
+    }
 }
