64f03294817b3434e8eafda483a37850344a1a91
F0rkster <matthias-gabel@hotmail.de>
2021-01-15 19:58:58 +0100

    Handle Exceptions

--- src/test/java/UserContainerTest.java
+++ src/test/java/UserContainerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -72,5 +73,56 @@ public class UserContainerTest {
 
         // Then
         assertThrows(PasswordException.class, () -> testUserContainer.registerUser(firstName, lastName, username, email, password, birthdate, sex));
+    }
+
+    @Test
+    public void activeUser_should_contain_all_active_user()
+    {
+        // Given
+        for (int i = 0; i < 100; i++)
+        {
+            switch (i)
+            {
+                case 5:
+                case 15:
+                case 30:
+                case 56:
+                case 78:
+                case 90:
+                    String firstName = "Active" + i;
+                    String lastName = "User" + i;
+                    String userName = "ActiveUser" + 1;
+                    String email = "ActUser" + i + "@email.de";
+                    String password = "ActiveUserPW" + i + "!";
+                    Date birthday = new Date(1999, Calendar.JUNE,22);
+                    PersonalInfo.sex sex = PersonalInfo.sex.uni;
+                    testUserContainer.registerUser(firstName, lastName, userName, email, password, birthday, sex);
+                    testUserContainer.getUsers().get(i).setOnlineStatusFlag(User.onlineStatus.online);
+                    break;
+                default:
+                    User user = new User();
+                    testUserContainer.getUsers().add(user);
+                    testUserContainer.getUsers().get(i).setOnlineStatusFlag(User.onlineStatus.offline);
+                    break;
+            }
+        }
+
+        ArrayList<User> activeUser = new ArrayList<User>();
+
+        // When
+            activeUser = testUserContainer.activeUser();
+        // Then
+        assertEquals("Active5", testUserContainer.getUsers().get(5).getPersonalInfo().getFirstName());
+        assertEquals("Active15", testUserContainer.getUsers().get(15).getPersonalInfo().getFirstName());
+        assertEquals("Active30", testUserContainer.getUsers().get(30).getPersonalInfo().getFirstName());
+        assertEquals("Active56", testUserContainer.getUsers().get(56).getPersonalInfo().getFirstName());
+        assertEquals("Active78", testUserContainer.getUsers().get(78).getPersonalInfo().getFirstName());
+        assertEquals("Active90", testUserContainer.getUsers().get(90).getPersonalInfo().getFirstName());
+        // Final
+        for (User user:
+             testUserContainer.getUsers()) {
+            user = null;
+        }
+        testUserContainer = null;
     }
 }
