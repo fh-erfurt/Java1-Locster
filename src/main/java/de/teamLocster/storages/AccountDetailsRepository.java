a521134b41533b4ba6a22d7e2c2faa83118f1f6a
Molham Al khodari <73823402+Molham321@users.noreply.github.com>
2021-05-13 12:15:39 +0200

    entity & repository done

--- src/main/java/de/teamLocster/storages/AccountDetailsRepository.java
+++ src/main/java/de/teamLocster/storages/AccountDetailsRepository.java
@@ -1,9 +1,11 @@
 package de.teamLocster.storages;
 
+import de.teamLocster.core.H2Controller;
 import de.teamLocster.domains.AccountDetails;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class AccountDetailsRepository extends BaseRepository<AccountDetails> {
-    public AccountDetailsRepository() {
-        super(AccountDetails.class, H2Controller.getManager().getEntityManager());
-    }
+
+    public AccountDetailsRepository() { super(AccountDetails.class, H2Controller.getManager().getEntityManager());}
 }
