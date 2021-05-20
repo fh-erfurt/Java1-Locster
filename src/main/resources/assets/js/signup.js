478eb51a800344c941a6ab28676016ab00fa871b
Munchkin129 <dirk.hofmann@fh-erfurt.de>
2021-05-20 17:59:18 +0200

    added passwordvalidator

--- src/main/resources/assets/js/signup.js
+++ src/main/resources/assets/js/signup.js
@@ -1,11 +1,39 @@
-let input;
+let inputPassword;
+let inputPasswordRepeat;
 let messageText;
 
 onload = function (event) {
-    input = document.getElementsByName('password')[0];
+    inputPassword = document.getElementsByName('password')[0];
+    inputPasswordRepeat = document.getElementsByName('passwordRepeat')[0];
 };
 
-function onPWChanged() {
-    const password = input.value;
-    console.log(password);
+function onPasswordChanged() {
+    const password = inputPassword.value;
+    if(password) {
+        var regexWeak = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/m;
+        var regexMedium = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/m;
+        var regexStrong = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/m;
+        let string = password;
+
+        if (string.match(regexStrong)) {
+            inputPassword.style.border = '2px solid green';
+        } else if (string.match(regexMedium)) {
+            inputPassword.style.border = '2px solid yellow';
+        } else if (string.match(regexWeak)) {
+            inputPassword.style.border = '2px solid orange';
+        } else {
+            inputPassword.style.border = '2px solid red';
+        }
+    }
+};
+
+
+function onPasswordRepeatChanged() {
+    const passwordRepeat = inputPasswordRepeat.value;
+    const password = inputPassword.value;
+            if (password === passwordRepeat) {
+                inputPasswordRepeat.style.border = '2px solid green';
+            } else {
+                inputPasswordRepeat.style.border = '2px solid red';
+            }
 };
