1657406a71a463c7ac9cac1d476e243b905081a8
F0rkster <matthias-gabel@hotmail.de>
2021-05-20 18:53:11 +0200

    final

--- src/main/resources/assets/js/signup.js
+++ src/main/resources/assets/js/signup.js
@@ -1,18 +1,27 @@
 let inputPassword;
 let inputPasswordRepeat;
+let major;
+let minor;
+let number;
+let special;
 let messageText;
 
-onload = function (event) {
+onload = function () {
     inputPassword = document.getElementsByName('password')[0];
     inputPasswordRepeat = document.getElementsByName('passwordRepeat')[0];
+    length = document.querySelector('.inputs div p .length-8');
+    major = document.querySelector('.inputs div p .major');
+    minor = document.querySelector('.inputs div p .minor');
+    number = document.querySelector('.inputs div p .number');
+    special = document.querySelector('.inputs div p .special')
 };
 
 function onPasswordChanged() {
     const password = inputPassword.value;
     if(password) {
-        var regexWeak = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/m;
-        var regexMedium = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/m;
-        var regexStrong = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/m;
+        let regexWeak = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/m;
+        let regexMedium = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/m;
+        let regexStrong = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/m;
         let string = password;
 
         if (string.match(regexStrong)) {
@@ -24,8 +33,14 @@ function onPasswordChanged() {
         } else {
             inputPassword.style.border = '2px solid red';
         }
+
+        password.length >= 8        ? length.style.color    = 'green'   : length.style.color  = 'red';
+        string.match(/[a-z]/m)      ? minor.style.color     = 'green'   : minor.style.color   = 'red';
+        string.match(/[A-Z]/m)      ? major.style.color     = 'green'   : major.style.color   = 'red';
+        string.match(/[0-9]/m)      ? number.style.color    = 'green'   : number.style.color  = 'red';
+        string.match(/[@$!%*?&]/m)  ? special.style.color   = 'green'   : special.style.color = 'red';
     }
-};
+}
 
 
 function onPasswordRepeatChanged() {
@@ -36,4 +51,4 @@ function onPasswordRepeatChanged() {
             } else {
                 inputPasswordRepeat.style.border = '2px solid red';
             }
-};
+}
