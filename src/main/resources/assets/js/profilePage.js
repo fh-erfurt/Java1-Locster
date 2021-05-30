e690ddbaf3041da64b50aafa6e42a20463c4107e
Molham Al khodari <73823402+Molham321@users.noreply.github.com>
2021-05-30 14:57:28 +0200

    collapsible

--- src/main/resources/assets/js/profilePage.js
+++ src/main/resources/assets/js/profilePage.js
@@ -1,20 +1,14 @@
-function handleProfileStatistics() {
-  console.log("function läuft");
-  let coll = document.getElementsByClassName("collapsible");
-  let i;
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-      this.classList.toggle("active");
-      //   let content = this.nextElementSibling;
-      let content = document.getElementsByClassName("collapsible_content");
-      if (content.style.display === "block") {
-        content.style.display = "none";
-        console.log("disolay none");
-      } else {
-        content.style.display = "block";
-        console.log("disolay block");
-      }
-    });
-  }
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
