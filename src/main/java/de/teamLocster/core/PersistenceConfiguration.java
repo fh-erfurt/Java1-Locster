d9c0ea307348af71e4c93c0c014ca93256ccd2dc
bro-scientist <jacxta@gmail.com>
2021-06-01 09:25:12 +0200

    LOC-16 refactoring

---
+++ src/main/java/de/teamLocster/core/PersistenceConfiguration.java
@@ -0,0 +1,13 @@
+package de.teamLocster.core;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+// @Import(SpringDocDataRestConfiguration.class)
+public class PersistenceConfiguration
+{
+}
