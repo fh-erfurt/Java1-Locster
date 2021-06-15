1fd7b713ced02ad8be7cb7b4d9c32ed7d707471d
F0rkster <matthias-gabel@hotmail.de>
2021-06-15 09:54:48 +0200

    move servingwbcontentapplication

--- src/main/java/de/teamLocster/ServingWebContentApplication.java
+++ src/main/java/de/teamLocster/ServingWebContentApplication.java
@@ -1,4 +1,4 @@
-package de.teamLocster.controller;
+package de.teamLocster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
