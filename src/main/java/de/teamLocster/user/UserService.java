20957751d111695df17589357a821fbd2700fae7
bro-scientist <jacxta@gmail.com>
2021-06-17 15:53:14 +0200

    user service configuration

--- src/main/java/de/teamLocster/user/UserService.java
+++ src/main/java/de/teamLocster/user/UserService.java
@@ -2,13 +2,13 @@ package de.teamLocster.user;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
-@SpringBootApplication
+@Service
 public class UserService {
 
     @Autowired
