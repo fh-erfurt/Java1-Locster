cbc63366cc072f431b60722e914852a8493c5638
Molham Al khodari <73823402+Molham321@users.noreply.github.com>
2021-06-21 14:49:40 +0200

    Login testing

--- src/main/java/de/teamLocster/controller/LoginController.java
+++ src/main/java/de/teamLocster/controller/LoginController.java
@@ -1,21 +1,36 @@
 package de.teamLocster.controller;
 
-
+import de.teamLocster.user.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.net.Authenticator;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
-    public String login() {
+    //to get login form page
+    @RequestMapping(value = "/login", method= RequestMethod.GET)
+    public String getLoginForm() {
+        //return html page name
         return "login";
     }
 
-//    @GetMapping(value = "/loginSuccessfull")
-//    public String currentUserName(Authenticaton authentication) {
-//        return "redirect:/";
-//    }
+    //checking for login credentials
+    @RequestMapping(value ="/login", method=RequestMethod.POST )
+    public String login(@ModelAttribute (name="User") User user, Model model) {
+
+        String email = user.getEMailAddress();
+        String password = user.getPasswordHash();
+
+        if("admin@gmail.com".equals(email) && "admin".equals(password)) {
+            // if username and password is correct, we are returning homepage
+            return "profilepage";
+        }
+        // if username or password is wrong
+        model.addAttribute("invalidCredentials", true);
+        //returning again login page
+        return "login";
+    }
 }
