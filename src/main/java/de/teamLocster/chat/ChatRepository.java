9f8b015348659cd84d3a003ef0f5e6b95f924224
bro-scientist <jacxta@gmail.com>
2021-07-05 15:01:27 +0200

    LOC-26 repositories

--- src/main/java/de/teamLocster/chat/ChatRepository.java
+++ src/main/java/de/teamLocster/chat/ChatRepository.java
@@ -2,7 +2,6 @@ package de.teamLocster.chat;
 
 import de.teamLocster.core.BaseRepository;
 import de.teamLocster.user.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
---
+++ src/main/java/de/teamLocster/guestbook/GuestbookEntryService.java
@@ -0,0 +1,11 @@
+package de.teamLocster.guestbook;
+
+import de.teamLocster.core.BaseService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class GuestbookEntryService extends BaseService<GuestbookEntry>
+{
+}
