97e356b8342b86ff393f5fbc24861f94b8fd4df1
Molham Al khodari <73823402+Molham321@users.noreply.github.com>
2021-07-27 12:55:51 +0200

    update ProfileContoller, add FindByEmail,

--- src/main/java/de/teamLocster/controller/ProfilepageController.java
+++ src/main/java/de/teamLocster/controller/ProfilepageController.java
@@ -1,16 +1,39 @@
 package de.teamLocster.controller;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import de.teamLocster.user.User;
+import de.teamLocster.user.UserRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class ProfilepageController {
     @GetMapping("/profilepage")
     public ModelAndView profilePage (Authentication authentication, Model model) {
         model.addAttribute("title", "Your Profile");
+        String userEmail = authentication.getName();
+        Optional<User> user = UserRepository.findByEmail(userEmail);
+        if (user.isPresent()){
+            model.addAttribute("firstName", user.get().getFirstName());
+            model.addAttribute("lastName", user.get().getLastName());
+            model.addAttribute("city", user.get().getRegion());
+            model.addAttribute("birthday", user.get().getBirthDay());
+            model.addAttribute("sex", user.get().getSex());
+            model.addAttribute("state", user.get().getRelationshipStatus());
+            model.addAttribute("city", user.get().getOccupation());
+            //ToDo: get Anzahl Besucher
+            //ToDo: get letzter Besucher
+            //ToDo: get neuster Freund
+            //ToDo: get ältester Freund
+
+        } else {
+            return new ModelAndView("redirect:/"); //ToDo Error page!
+        }
         return new ModelAndView("profilepage");
     }
+
 }
--- src/main/java/de/teamLocster/user/UserRepository.java
+++ src/main/java/de/teamLocster/user/UserRepository.java
@@ -3,9 +3,12 @@ package de.teamLocster.user;
 import de.teamLocster.core.BaseRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends BaseRepository<User>
 {
+    Optional<User> findByEmail(String email);
+
     List<User> findByIsOnlineTrue();
 
     User findByEmailAddress(String emailAddress);
