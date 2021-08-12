6033836802fddf6e2edbe9abc4eb6c6e4190f3c3
bro-scientist <jacxta@gmail.com>
2021-08-12 18:51:19 +0200

    new query for whoisonline

--- src/main/java/de/teamLocster/user/UserRepository.java
+++ src/main/java/de/teamLocster/user/UserRepository.java
@@ -1,6 +1,7 @@
 package de.teamLocster.user;
 
 import de.teamLocster.core.BaseRepository;
+import de.teamLocster.enums.OnlineStatus;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,6 +9,8 @@ import java.util.Optional;
 public interface UserRepository extends BaseRepository<User>
 {
     List<User> findByIsOnlineTrue();
+
+    List<User> findByIsOnlineTrueAndOnlineStatusNot(OnlineStatus onlineStatus);
 
     Optional<User> findByEmailAddress(String emailAddress);
 }
