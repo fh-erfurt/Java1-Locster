0ff127c60b7d64bb926f20edc37b19efc90f36c2
F0rkster <matthias-gabel@hotmail.de>
2021-08-15 12:08:44 +0200

    delete search.js

--- src/main/resources/static/js/search.js
+++
@@ -1,7 +0,0 @@
-
-function onSearchbarChanged() {
-    var searchBar = document.getElementById("searchBar")
-    var value = encodeURI(searchBar.value)
-    var query = "/search?q="+value
-    console.log(query)
-}
